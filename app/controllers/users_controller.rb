8473059686b5b33ef4d7364478a075aa4618c5cf
WillEdgington <willedge037@gmail.com>
2025-10-03 11:58:18 +0100

    controller and routes layout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def show
+  end
+
+  def follow
+  end
+
+  def unfollow
+  end
+
+  def accept_follow
+  end
+
+  def reject_follow
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,31 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :users, only: [:index, :show]
 
   authenticate :user do
-    root "users#index", as: :authenticated_root
+    root "posts#index", as: :authenticated_root
   end
 
   unauthenticated do
     root "devise/sessions#new", as: :unauthenticated_root
   end
+
+  resources :users, only: [:index, :show] do
+    member do
+      post :follow
+      delete :unfollow
+      post :accept_follow
+      delete :reject_follow
+    end
+  end
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :commments, only: [] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
+  end
+
+  resources :likes, only: [:create, :destroy]
 end
